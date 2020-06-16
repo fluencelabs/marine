3efc3be345c500bcbbe71f8dae4652810c7454ea
vms <michail.vms@gmail.com>
2020-06-16 13:18:37 +0300

    enable circleci

--- fluence-faas/src/misc/utils.rs
+++ fluence-faas/src/misc/utils.rs
@@ -103,9 +103,7 @@ where
 }
 
 /// Make FCE config based on parsed raw config.
-pub(crate) fn make_fce_config(
-    config: Option<ModuleConfig>,
-) -> Result<FCEModuleConfig, FaaSError> {
+pub(crate) fn make_fce_config(config: Option<ModuleConfig>) -> Result<FCEModuleConfig, FaaSError> {
     use super::imports::create_host_import_func;
     use super::imports::log_utf8_string;
     use wasmer_core::import::Namespace;
