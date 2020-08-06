b8175590203a12841002e52f6c72f8c602480374
vms <michail.vms@gmail.com>
2020-08-06 23:50:37 +0300

    imrpove argument parsing

--- tools/repl/src/main.rs
+++ tools/repl/src/main.rs
@@ -27,6 +27,17 @@
 /// Command-line tool intended to test Fluence FaaS.
 use std::fs;
 
+macro_rules! next_argument {
+    ($arg_name:ident, $args:ident, $error_msg:expr) => {
+        let $arg_name = if let Some($arg_name) = $args.next() {
+            $arg_name
+        } else {
+            println!($error_msg);
+            continue;
+        };
+    };
+}
+
 fn main() -> Result<(), anyhow::Error> {
     println!("Welcome to the Fluence FaaS REPL:");
     let mut rl = rustyline::Editor::<()>::new();
@@ -37,18 +48,20 @@ fn main() -> Result<(), anyhow::Error> {
         match readline {
             Ok(line) => {
                 // TODO: improve argument parsing
-                let cmd: Vec<_> = line.split(' ').collect();
-                match cmd[0] {
-                    "load" => {
-                        let module_name = cmd[1].to_string();
-                        let wasm_bytes = fs::read(cmd[2]);
+                let mut args = line.split(' ');
+                match args.next() {
+                    Some("load") => {
+                        next_argument!(module_name, args, "Module name should be specified");
+                        next_argument!(module_path, args, "Module path should be specified");
+
+                        let wasm_bytes = fs::read(module_path);
                         if let Err(e) = wasm_bytes {
                             println!("failed to read wasm module: {}", e);
                             continue;
                         }
 
-                        let result_msg = match app_service.load_module(
-                            module_name,
+                        let result_msg = match app_service.load_module::<String, fluence_faas_service::ModuleConfig>(
+                            module_name.into(),
                             &wasm_bytes.unwrap(),
                             None,
                         ) {
@@ -57,25 +70,36 @@ fn main() -> Result<(), anyhow::Error> {
                         };
                         println!("{}", result_msg);
                     }
-                    "unload" => {
-                        let module_name = cmd[1];
+                    Some("unload") => {
+                        next_argument!(module_name, args, "Module name should be specified");
+
                         let result_msg = match app_service.unload_module(module_name) {
                             Ok(_) => "module successfully unloaded from App service".to_string(),
                             Err(e) => format!("module unloaded failed with: {:?}", e),
                         };
                         println!("{}", result_msg);
                     }
-                    "call" => {
-                        let module_name = cmd[1];
-                        let func_name = cmd[2];
-                        let arg = cmd[3..].join(" ");
-                        let result = match app_service.call(module_name, func_name, arg.as_bytes()) {
+                    Some("call") => {
+                        next_argument!(module_name, args, "Module name should be specified");
+                        next_argument!(func_name, args, "Function name should be specified");
+
+                        let module_arg: String = args.collect();
+                        let module_arg: serde_json::Value = match serde_json::from_str(&module_arg) {
+                            Ok(module_arg) => module_arg,
+                            Err(e) => {
+                                println!("incorrect arguments {}", e);
+                                continue;
+                            },
+                        };
+
+                        let result = match app_service.call(module_name, func_name, module_arg)
+                        {
                             Ok(result) => format!("result: {:?}", result),
                             Err(e) => format!("execution failed with {:?}", e),
                         };
                         println!("{}", result);
                     }
-                    "help" => {
+                    Some("help") | None => {
                         println!(
                             "Enter:\n\
                                 load <module_name> <module_path>        - to load a new Wasm module into App service\n\
@@ -85,7 +109,7 @@ fn main() -> Result<(), anyhow::Error> {
                                 e/exit/q/quit                           - to exit"
                         );
                     }
-                    "e" | "exit" | "q" | "quit" => break,
+                    Some("e") | Some("exit") | Some("q") | Some("quit") => break,
                     _ => {
                         println!("unsupported command");
                     }
@@ -100,3 +124,23 @@ fn main() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
+
+/*
+fn call_function_command(
+    app_service: &mut FluenceFaaSService,
+    args: &[String],
+) -> Result<serde_json::Value, String> {
+    let module_name = &args[0];
+    let func_name = &args[1];
+    let module_interfaces = app_service
+        .get_interface()
+        .modules
+        .get(module_name)
+        .ok_or_else(|| Err(format!("{} not found in app service", module_name)))?;
+    let function_signature = module_interfaces
+        .get(module_name)
+        .ok_or_else(|| Err(format!("{} not found in {} module", func_name, module_name)))?;
+
+    //    let arguments = serde_json::from
+}
+*/
