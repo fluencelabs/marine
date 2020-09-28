e30f502574a032fc1900a8d331b83b2939312ed6
folex <0xdxdy@gmail.com>
2020-09-28 15:07:51 +0300

    reimport IValue, IType

--- aquamarine-vm/src/lib.rs
+++ aquamarine-vm/src/lib.rs
@@ -30,6 +30,8 @@ mod errors;
 pub use aquamarine_vm::AquamarineVM;
 pub use aquamarine_vm::StepperOutcome;
 pub use errors::AquamarineVMError;
-pub use fluence_faas::HostImportDescriptor;
+
+/// Reimports
+pub use fluence_faas::{HostImportDescriptor, HostImportError, IValue, IType};
 
 pub(crate) type Result<T> = std::result::Result<T, AquamarineVMError>;
