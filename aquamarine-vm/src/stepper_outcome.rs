1c2578ede462430d6d15e6c750a515600342f69d
vms <michail.vms@gmail.com>
2020-10-08 11:41:00 +0300

    update stepper errors

--- aquamarine-vm/src/stepper_outcome.rs
+++ aquamarine-vm/src/stepper_outcome.rs
@@ -22,44 +22,62 @@ use crate::AquamarineVMError;
 use std::convert::TryFrom;
 use std::error::Error;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub(crate) struct RawStepperOutcome {
     pub ret_code: i32,
     pub data: String,
     pub next_peer_pks: Vec<String>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct StepperOutcome {
     pub data: String,
     pub next_peer_pks: Vec<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum StepperError {
     /// Errors occurred while parsing aqua script in the form of S expressions.
     SExprParseError(String),
 
     /// Errors occurred while parsing supplied data.
-    DataParseError(String),
+    DataSerdeError(String),
+
+    /// Errors occurred while parsing function arguments of an expression.
+    FuncArgsSerdeError(String),
+
+    /// Errors occurred while parsing returned by call_service value.
+    CallServiceSerdeError(String),
 
     /// Indicates that environment variable with name CURRENT_PEER_ID isn't set.
-    CurrentPeerIdNotSet(String),
+    CurrentPeerIdEnvError(String),
 
     /// Semantic errors in instructions.
     InstructionError(String),
 
-    /// Semantic errors in instructions.
+    /// An error is occurred while calling local service via call_service.
     LocalServiceError(String),
 
-    /// Value with such name isn't presence in data.
+    /// Value for such name isn't presence in data.
     VariableNotFound(String),
 
-    /// Value with such path wasn't found in data.
+    /// Multiple values for such name found.
+    MultipleVariablesFound(String),
+
+    /// Value with such path wasn't found in data with such error.
     VariableNotInJsonPath(String),
+
+    /// Value for such name isn't presence in data.
+    IncompatibleJValueType(String),
 
     /// Multiple values found for such json path.
     MultipleValuesInJsonPath(String),
+
+    /// Fold state wasn't found for such iterator name.
+    FoldStateNotFound(String),
+
+    /// Multiple fold states found for such iterator name.
+    MultipleFoldStates(String),
 
     /// Related to such ret_code that doesn't have match with current StepperError.
     UnknownError(String),
@@ -71,13 +89,19 @@ impl std::fmt::Display for StepperError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
             StepperError::SExprParseError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::DataParseError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::CurrentPeerIdNotSet(err_msg) => write!(f, "{}", err_msg),
+            StepperError::DataSerdeError(err_msg) => write!(f, "{}", err_msg),
+            StepperError::FuncArgsSerdeError(err_msg) => write!(f, "{}", err_msg),
+            StepperError::CallServiceSerdeError(err_msg) => write!(f, "{}", err_msg),
+            StepperError::CurrentPeerIdEnvError(err_msg) => write!(f, "{}", err_msg),
             StepperError::InstructionError(err_msg) => write!(f, "{}", err_msg),
             StepperError::LocalServiceError(err_msg) => write!(f, "{}", err_msg),
             StepperError::VariableNotFound(err_msg) => write!(f, "{}", err_msg),
+            StepperError::MultipleVariablesFound(err_msg) => write!(f, "{}", err_msg),
             StepperError::VariableNotInJsonPath(err_msg) => write!(f, "{}", err_msg),
+            StepperError::IncompatibleJValueType(err_msg) => write!(f, "{}", err_msg),
             StepperError::MultipleValuesInJsonPath(err_msg) => write!(f, "{}", err_msg),
+            StepperError::FoldStateNotFound(err_msg) => write!(f, "{}", err_msg),
+            StepperError::MultipleFoldStates(err_msg) => write!(f, "{}", err_msg),
             StepperError::UnknownError(err_msg) => write!(f, "{}", err_msg),
         }
     }
@@ -101,13 +125,19 @@ impl TryFrom<RawStepperOutcome> for StepperOutcome {
                 next_peer_pks: raw_outcome.next_peer_pks,
             }),
             1 => to_vm_error!(SExprParseError),
-            2 => to_vm_error!(DataParseError),
-            3 => to_vm_error!(CurrentPeerIdNotSet),
-            4 => to_vm_error!(InstructionError),
-            5 => to_vm_error!(LocalServiceError),
-            6 => to_vm_error!(VariableNotFound),
-            7 => to_vm_error!(VariableNotInJsonPath),
-            8 => to_vm_error!(MultipleValuesInJsonPath),
+            2 => to_vm_error!(DataSerdeError),
+            3 => to_vm_error!(FuncArgsSerdeError),
+            4 => to_vm_error!(CallServiceSerdeError),
+            5 => to_vm_error!(CurrentPeerIdEnvError),
+            6 => to_vm_error!(InstructionError),
+            7 => to_vm_error!(LocalServiceError),
+            8 => to_vm_error!(VariableNotFound),
+            9 => to_vm_error!(MultipleVariablesFound),
+            10 => to_vm_error!(VariableNotInJsonPath),
+            11 => to_vm_error!(IncompatibleJValueType),
+            12 => to_vm_error!(MultipleValuesInJsonPath),
+            13 => to_vm_error!(FoldStateNotFound),
+            14 => to_vm_error!(MultipleFoldStates),
             _ => to_vm_error!(UnknownError),
         }
     }
