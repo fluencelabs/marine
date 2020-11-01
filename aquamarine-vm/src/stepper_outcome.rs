f4680075f411a605844ffb3ef7f28eda053c2e50
vms <michail.vms@gmail.com>
2020-11-01 10:16:21 +0300

    adjust AquamarineVM error mapping

--- aquamarine-vm/src/stepper_outcome.rs
+++ aquamarine-vm/src/stepper_outcome.rs
@@ -25,86 +25,77 @@ use std::error::Error;
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub(crate) struct RawStepperOutcome {
     pub ret_code: i32,
-    pub data: String,
-    pub next_peer_pks: Vec<String>,
+    pub data: err_msg,
+    pub next_peer_pks: Vec<err_msg>,
 }
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct StepperOutcome {
-    pub data: String,
-    pub next_peer_pks: Vec<String>,
+    pub data: err_msg,
+    pub next_peer_pks: Vec<err_msg>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum StepperError {
     /// Errors occurred while parsing aqua script in the form of S expressions.
-    SExprParseError(String),
-
-    /// Errors occurred on aqua data deserialization.
-    DataDeserializationError(String),
-
-    /// Errors occurred on aqua data serialization.
-    DataSerializationError(String),
+    SExprParseError(err_msg),
 
     /// Errors occurred while parsing function arguments of an expression.
-    FuncArgsSerdeError(String),
+    FuncArgsSerializationError(err_msg),
 
     /// Errors occurred while parsing returned by call_service value.
-    CallServiceSerdeError(String),
+    CallServiceResultDeserializationError(err_msg),
 
     /// Indicates that environment variable with name CURRENT_PEER_ID isn't set.
-    CurrentPeerIdEnvError(String),
+    CurrentPeerIdEnvError(err_msg),
 
     /// Semantic errors in instructions.
-    InstructionError(String),
+    InstructionError(err_msg),
 
     /// An error is occurred while calling local service via call_service.
-    LocalServiceError(String),
+    LocalServiceError(err_msg),
 
     /// Value for such name isn't presence in data.
-    VariableNotFound(String),
+    VariableNotFound(err_msg),
 
     /// Multiple values for such name found.
-    MultipleVariablesFound(String),
+    MultipleVariablesFound(err_msg),
 
     /// Value with such path wasn't found in data with such error.
-    VariableNotInJsonPath(String),
+    VariableNotInJsonPath(err_msg),
 
-    /// Value for such name isn't presence in data.
-    IncompatibleJValueType(String),
+    /// Provided JValue has incompatible with target type.
+    IncompatibleJValueType(err_msg),
+
+    /// Provided AValue has incompatible with target type.
+    IncompatibleAValueType(err_msg),
 
     /// Multiple values found for such json path.
-    MultipleValuesInJsonPath(String),
+    MultipleValuesInJsonPath(err_msg),
 
     /// Fold state wasn't found for such iterator name.
-    FoldStateNotFound(String),
+    FoldStateNotFound(err_msg),
 
     /// Multiple fold states found for such iterator name.
-    MultipleFoldStates(String),
+    MultipleFoldStates(err_msg),
 
     /// Expected evidence state of different type.
-    InvalidEvidenceState(String),
-
-    /// Related to such ret_code that doesn't have match with current StepperError.
-    UnknownError(String),
+    InvalidEvidenceState(err_msg),
 
     /// Errors occurred on call evidence deserialization.
-    CallEvidenceDeserializationError(String),
+    CallEvidenceDeserializationError(err_msg),
 
     /// Errors occurred on call evidence serialization.
-    CallEvidenceSerializationError(String),
-
-    /// Errors occurred when reserved keyword is used for variable name.
-    ReservedKeywordError(String),
+    CallEvidenceSerializationError(err_msg),
 
     /// Errors occurred when previous and current evidence states are incompatible.
-    IncompatibleEvidenceStates(String),
+    IncompatibleEvidenceStates(err_msg),
 
     /// Errors occurred when previous and current call results are incompatible.
-    IncompatibleCallResults(String),
+    IncompatibleCallResults(err_msg),
 
     /// Errors occurred when evidence path contains less elements then corresponding Par has.
-    EvidencePathTooSmall(String),
+    EvidencePathTooSmall(err_msg),
 }
 
 impl Error for StepperError {}
@@ -112,29 +103,28 @@ impl Error for StepperError {}
 impl std::fmt::Display for StepperError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-            StepperError::SExprParseError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::DataDeserializationError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::DataSerializationError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::FuncArgsSerdeError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::CallServiceSerdeError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::CurrentPeerIdEnvError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::InstructionError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::LocalServiceError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::VariableNotFound(err_msg) => write!(f, "{}", err_msg),
-            StepperError::MultipleVariablesFound(err_msg) => write!(f, "{}", err_msg),
-            StepperError::VariableNotInJsonPath(err_msg) => write!(f, "{}", err_msg),
-            StepperError::IncompatibleJValueType(err_msg) => write!(f, "{}", err_msg),
-            StepperError::MultipleValuesInJsonPath(err_msg) => write!(f, "{}", err_msg),
-            StepperError::FoldStateNotFound(err_msg) => write!(f, "{}", err_msg),
-            StepperError::MultipleFoldStates(err_msg) => write!(f, "{}", err_msg),
-            StepperError::InvalidEvidenceState(err_msg) => write!(f, "{}", err_msg),
-            StepperError::CallEvidenceDeserializationError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::CallEvidenceSerializationError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::ReservedKeywordError(err_msg) => write!(f, "{}", err_msg),
-            StepperError::IncompatibleEvidenceStates(err_msg) => write!(f, "{}", err_msg),
-            StepperError::IncompatibleCallResults(err_msg) => write!(f, "{}", err_msg),
-            StepperError::EvidencePathTooSmall(err_msg) => write!(f, "{}", err_msg),
-            StepperError::UnknownError(err_msg) => write!(f, "{}", err_msg),
+            StepperError::SExprParseError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::FuncArgsSerializationError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::CallServiceResultDeserializationError(err_msg) => {
+                writeln!(f, "{}", err_msg)
+            }
+            StepperError::CurrentPeerIdEnvError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::InstructionError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::LocalServiceError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::VariableNotFound(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::MultipleVariablesFound(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::VariableNotInJsonPath(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::IncompatibleJValueType(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::IncompatibleAValueType(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::MultipleValuesInJsonPath(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::FoldStateNotFound(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::MultipleFoldStates(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::InvalidEvidenceState(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::CallEvidenceDeserializationError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::CallEvidenceSerializationError(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::IncompatibleEvidenceStates(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::IncompatibleCallResults(err_msg) => writeln!(f, "{}", err_msg),
+            StepperError::EvidencePathTooSmall(err_msg) => writeln!(f, "{}", err_msg),
         }
     }
 }
@@ -157,26 +147,25 @@ impl TryFrom<RawStepperOutcome> for StepperOutcome {
                 next_peer_pks: raw_outcome.next_peer_pks,
             }),
             1 => to_vm_error!(SExprParseError),
-            2 => to_vm_error!(DataDeserializationError),
-            3 => to_vm_error!(DataSerializationError),
-            4 => to_vm_error!(FuncArgsSerdeError),
-            5 => to_vm_error!(CallServiceSerdeError),
-            6 => to_vm_error!(CurrentPeerIdEnvError),
-            7 => to_vm_error!(InstructionError),
-            8 => to_vm_error!(LocalServiceError),
-            9 => to_vm_error!(VariableNotFound),
-            10 => to_vm_error!(MultipleVariablesFound),
-            11 => to_vm_error!(VariableNotInJsonPath),
-            12 => to_vm_error!(IncompatibleJValueType),
-            13 => to_vm_error!(MultipleValuesInJsonPath),
-            14 => to_vm_error!(FoldStateNotFound),
-            15 => to_vm_error!(MultipleFoldStates),
+            2 => to_vm_error!(FuncArgsSerializationError),
+            3 => to_vm_error!(CallServiceResultDeserializationError),
+            4 => to_vm_error!(CurrentPeerIdEnvError),
+            5 => to_vm_error!(InstructionError),
+            6 => to_vm_error!(LocalServiceError),
+            7 => to_vm_error!(VariableNotFound),
+            8 => to_vm_error!(MultipleVariablesFound),
+            9 => to_vm_error!(VariableNotInJsonPath),
+            10 => to_vm_error!(IncompatibleJValueType),
+            11 => to_vm_error!(IncompatibleAValueType),
+            12 => to_vm_error!(MultipleValuesInJsonPath),
+            13 => to_vm_error!(FoldStateNotFound),
+            14 => to_vm_error!(MultipleFoldStates),
+            15 => to_vm_error!(InvalidEvidenceState),
             16 => to_vm_error!(CallEvidenceDeserializationError),
             17 => to_vm_error!(CallEvidenceSerializationError),
-            18 => to_vm_error!(ReservedKeywordError),
-            19 => to_vm_error!(IncompatibleEvidenceStates),
-            20 => to_vm_error!(IncompatibleCallResults),
-            21 => to_vm_error!(EvidencePathTooSmall),
+            18 => to_vm_error!(IncompatibleEvidenceStates),
+            19 => to_vm_error!(IncompatibleCallResults),
+            20 => to_vm_error!(EvidencePathTooSmall),
             _ => to_vm_error!(UnknownError),
         }
     }
