1569c7dd99813ae9eb2da1f7fb986a91d463eca2
vms <michail.vms@gmail.com>
2020-11-21 01:31:48 +0300

    use serde serializer for interfaces

--- fluence-faas/src/faas_interface.rs
+++ fluence-faas/src/faas_interface.rs
@@ -17,40 +17,22 @@
 use super::IType;
 use super::IRecordType;
 use crate::FaaSModuleInterface;
-use crate::FaaSFunctionSignature;
 
 use fce::RecordTypes;
+use itertools::Itertools;
 use serde::Serialize;
-use serde::Serializer;
 
 use std::fmt;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::rc::Rc;
-use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct FaaSInterface<'a> {
     pub modules: HashMap<&'a str, FaaSModuleInterface<'a>>,
 }
 
 impl<'a> fmt::Display for FaaSInterface<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fn type_text_view(arg_ty: &IType, record_types: &RecordTypes) -> String {
-            match arg_ty {
-                IType::Record(record_type_id) => {
-                    // unwrap is safe because FaaSInterface here is well-formed
-                    // (it was checked on the module startup stage)
-                    let record = record_types.get(record_type_id).unwrap();
-                    record.name.clone()
-                }
-                IType::Array(array_ty) => {
-                    format!("Array<{}>", type_text_view(array_ty, record_types))
-                }
-                t => format!("{:?}", t),
-            }
-        };
-
         let mut printed_record_types: HashSet<&IRecordType> = HashSet::new();
 
         for (_, module_interface) in self.modules.iter() {
@@ -114,92 +96,15 @@ impl<'a> fmt::Display for FaaSInterface<'a> {
     }
 }
 
-impl<'a> Serialize for FaaSInterface<'a> {
-    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
-    where
-        S: Serializer,
-    {
-        #[derive(Serialize)]
-        pub struct FunctionSignature<'a> {
-            pub name: &'a Rc<String>,
-            pub arguments: Vec<(&'a String, &'a IType)>,
-            pub output_types: &'a Rc<Vec<IType>>,
+fn type_text_view(arg_ty: &IType, record_types: &RecordTypes) -> String {
+    match arg_ty {
+        IType::Record(record_type_id) => {
+            // unwrap is safe because FaaSInterface here is well-formed
+            // (it was checked on the module startup stage)
+            let record = record_types.get(record_type_id).unwrap();
+            record.name.clone()
         }
-
-        #[derive(Serialize)]
-        pub struct RecordType<'a> {
-            pub name: &'a str,
-            pub id: u64,
-            pub fields: Vec<(&'a String, &'a IType)>,
-        }
-
-        #[derive(Serialize)]
-        pub struct Module<'a> {
-            pub name: &'a str,
-            pub function_signatures: Vec<FunctionSignature<'a>>,
-            pub record_types: Vec<RecordType<'a>>,
-        }
-
-        #[derive(Serialize)]
-        pub struct Interface<'a> {
-            pub modules: Vec<Module<'a>>,
-        }
-
-        fn serialize_function_signature(
-            signature: &FaaSFunctionSignature,
-        ) -> FunctionSignature<'_> {
-            let arguments = signature
-                .arguments
-                .iter()
-                .map(|arg| (&arg.name, &arg.ty))
-                .collect();
-
-            FunctionSignature {
-                name: &signature.name,
-                arguments,
-                output_types: &signature.outputs,
-            }
-        }
-
-        fn serialize_record_type<'a, 'b>(record: (&'a u64, &'b Rc<IRecordType>)) -> RecordType<'b> {
-            let fields = record
-                .1
-                .fields
-                .iter()
-                .map(|field| (&field.name, &field.ty))
-                .collect::<Vec<_>>();
-
-            RecordType {
-                name: record.1.name.as_str(),
-                id: *record.0,
-                fields,
-            }
-        }
-
-        let modules: Vec<_> = self
-            .modules
-            .iter()
-            .map(|(name, interface)| {
-                let function_signatures = interface
-                    .function_signatures
-                    .iter()
-                    .map(serialize_function_signature)
-                    .collect();
-
-                let record_types: Vec<_> = interface
-                    .record_types
-                    .iter()
-                    .map(serialize_record_type)
-                    .collect();
-
-                Module {
-                    name,
-                    function_signatures,
-                    record_types,
-                }
-            })
-            .collect();
-
-        Interface { modules }.serialize(serializer)
+        IType::Array(array_ty) => format!("Array<{}>", type_text_view(array_ty, record_types)),
+        t => format!("{:?}", t),
     }
 }
