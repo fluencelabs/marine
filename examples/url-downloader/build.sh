de9e219d086df1681a102c87dd1d156f5a21698b
vms <michail.vms@gmail.com>
2020-12-09 13:38:06 +0300

    update build.sh in url service

--- examples/url-downloader/build.sh
+++ examples/url-downloader/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # This script builds all subprojects and puts all created Wasm modules in one dir
-cd curl_adapter
+cd local_storage
 cargo update
 fce build --release
-cd ../local_storage
+cd ../curl_adapter
 cargo update
 fce build --release
 cd ../facade
@@ -13,6 +13,6 @@ fce build --release
 
 cd ..
 rm -f artifacts/*
-cp ../../target/wasm32-wasi/release/curl_adapter.wasm artifacts/
 cp ../../target/wasm32-wasi/release/local_storage.wasm artifacts/
+cp ../../target/wasm32-wasi/release/curl_adapter.wasm artifacts/
 cp ../../target/wasm32-wasi/release/facade.wasm artifacts/
