9237a12598dbe050faf83ffac2b00fdaf571a505
vms <michail.vms@gmail.com>
2020-12-29 14:09:29 +0300

    fix imports

--- fluence-faas/tests/records.rs
+++ fluence-faas/tests/records.rs
@@ -57,7 +57,7 @@ pub fn records() {
     assert_eq!(
         result1,
         vec![IValue::Record(
-            wasmer_wit::vec1::Vec1::new(vec![
+            wasmer_wit::NEVec::new(vec![
                 IValue::I32(1),
                 IValue::S8(1),
                 IValue::S16(2),
