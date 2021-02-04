65a72238032f6d875b24a26828cf388cead830c8
Dima <dmitry.shakhtarin@fluence.ai>
2021-02-04 15:32:45 +0300

    update sqlite, update all wasms, small fixes (#60)
    Co-authored-by: vms <michail.vms@gmail.com>

--- examples/sqlite/build.sh
+++ examples/sqlite/build.sh
@@ -6,5 +6,5 @@ fce build --release
 
 rm artifacts/*
 cp ../../target/wasm32-wasi/release/sqlite_test.wasm artifacts/
-wget https://github.com/fluencelabs/sqlite/releases/download/v0.8.0_w/sqlite3.wasm
+wget https://github.com/fluencelabs/sqlite/releases/download/v0.9.0_w/sqlite3.wasm
 mv sqlite3.wasm artifacts/
