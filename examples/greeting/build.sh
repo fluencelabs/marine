3b0f6b80b7f41ff65efaf75f1fb5765c76350672
vms <michail.vms@gmail.com>
2021-03-12 20:00:36 +0300

    revert back greeting building script

--- examples/greeting/build.sh
+++ examples/greeting/build.sh
@@ -2,7 +2,7 @@
 
 # This script builds all subprojects and puts all created Wasm modules in one dir
 cargo update
-../../target/debug/fce build --release
+fce build --release
 
 rm artifacts/*
 cp ../../target/wasm32-wasi/release/greeting.wasm artifacts/
