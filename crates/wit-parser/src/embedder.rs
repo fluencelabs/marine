abfbe6aba5ecff7668d28e53bfe4cb622b9e8411
vms <michail.vms@gmail.com>
2021-03-15 18:45:00 +0300

    use wasmer-wit 0.18.3

--- crates/wit-parser/src/embedder.rs
+++ crates/wit-parser/src/embedder.rs
@@ -1,11 +1,11 @@
 /*
- * Copyright 2wasmer-wit = { package = "wasmer-interface-types-fl", version = "=0.18.0" }2wasmer-wit = { package = "wasmer-interface-types-fl", version = "=0.18.0" } Fluence Labs Limited
+ * Copyright 2020 Fluence Labs Limited
  *
- * Licensed under the Apache License, Version 2.wasmer-wit = { package = "wasmer-interface-types-fl", version = "=0.18.0" } (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.wasmer-wit = { package = "wasmer-interface-types-fl", version = "=0.18.0" }
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
