aeb9c9c5020c4f79fecbac9c8387607bd4d635a1
folex <0xdxdy@gmail.com>
2021-06-03 20:08:18 +0300

    fix compilation

--- crates/module-interface/src/interface/module_interface.rs
+++ crates/module-interface/src/interface/module_interface.rs
@@ -70,8 +70,8 @@ impl fmt::Display for FunctionSignature {
         use itertools::Itertools;
 
         let (designator, output) = match self.output_types.len() {
-            0 => ("", String::new()),
-            1 => ("->", &self.output_types[0]),
+            0 => ("", ""),
+            1 => ("->", self.output_types[0].as_str()),
             _ => unimplemented!("more than 1 output type is unsupported"),
         };
 
