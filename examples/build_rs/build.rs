2585f52420153d54108bbd69bdf37c54b156523c
Valery Antopol <valery.antopol@gmail.com>
2021-10-15 05:50:16 +0300

    add marine-rs-sdk-test examples for build.rs and mod

---
+++ examples/build_rs/build.rs
@@ -0,0 +1,16 @@
+use marine_rs_sdk_test::generate_marine_test_env;
+use marine_rs_sdk_test::ServiceDescription;
+fn main() {
+    let services = vec![
+        ("greeting".to_string(), ServiceDescription {
+            config_path: "Config.toml".to_string(),
+            modules_dir: Some("artifacts".to_string()),
+        })
+    ];
+
+    if cfg!(not(target_arch = "wasm32")) {
+        generate_marine_test_env(&services, "marine_test_env.rs", file!());
+    }
+
+    println!("cargo:rerun-if-changed=src/main.rs");
+}
