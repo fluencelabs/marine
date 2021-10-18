841d3cf7c56bd388354153986407e4a490f9f19b
Valery Antopol <valery.antopol@gmail.com>
2021-10-18 20:05:14 +0300

    fmt

--- examples/build_rs/build.rs
+++ examples/build_rs/build.rs
@@ -1,12 +1,13 @@
 use marine_rs_sdk_test::generate_marine_test_env;
 use marine_rs_sdk_test::ServiceDescription;
 fn main() {
-    let services = vec![
-        ("greeting".to_string(), ServiceDescription {
+    let services = vec![(
+        "greeting".to_string(),
+        ServiceDescription {
             config_path: "Config.toml".to_string(),
             modules_dir: Some("artifacts".to_string()),
-        })
-    ];
+        },
+    )];
 
     let target = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     if target != "wasm32" {
