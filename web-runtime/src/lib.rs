69781419649d415e70d95899f80c1df301c70a44
Valery Antopol <valery.antopol@gmail.com>
2022-01-20 03:08:19 +0300

    remove some dead code from web runtime

--- web-runtime/src/lib.rs
+++ web-runtime/src/lib.rs
@@ -38,11 +38,7 @@ mod config;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
-//use wasmer_it::ast::Interfaces;
-//use thiserror::Error as ThisError;
 use module::MModule;
-
-//use marine_js::*;
 
 pub use engine::MModuleInterface;
 pub use engine::Marine;
@@ -58,7 +54,6 @@ pub use module::to_interface_value;
 pub use wasmer_it::IRecordFieldType;
 pub use config::MModuleConfig;
 pub use config::HostImportDescriptor;
-//pub use config::HostExportedFunc;
 use crate::faas::FluenceFaaS;
 use marine_rs_sdk::CallParameters;
 
@@ -66,7 +61,7 @@ use once_cell::sync::Lazy;
 
 use std::str::FromStr;
 pub use wasmer_it::ne_vec;
-//use crate::module::type_converters::ival_to_string;
+
 
 pub(crate) type MResult<T> = std::result::Result<T, MError>;
 
@@ -86,12 +81,6 @@ pub fn min_it_version() -> &'static semver::Version {
     Lazy::force(&MINIMAL_SUPPORTED_IT_VERSION)
 }
 
-// Common JS stuff
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
-
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
@@ -103,42 +92,8 @@ pub fn js_log(_s: &str) {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-/*
-#[wasm_bindgen]
-pub fn test_call_export() {
-    call_export("greeting", "test_export", "[0]");
-}
-
-#[wasm_bindgen]
-pub fn test_read_memory() {
-    let result: u8 = read_byte("greeting", 0);
-    log(&result.to_string())
-}
-
-#[wasm_bindgen]
-pub fn test_write_memory() {
-    write_byte("greeting", 0, 42);
-}
-
-#[wasm_bindgen]
-pub fn test_it_section(bytes: &[u8]) {
-    let interfaces = extract_it_from_bytes(bytes);
-    let result = match interfaces {
-        Ok(interfaces) => interfaces.version.to_string(),
-        Err(e) => e.to_string(),
-    };
-
-    log(&result)
-}
-*/
-#[wasm_bindgen]
-pub fn register_module(name: &str, wit_section_bytes: &[u8], wasm_instance: JsValue) {
+pub fn register_module(name: &str, wit_section_bytes: &[u8], wasm_instance: JsValue) -> String {
     console_error_panic_hook::set_once();
-    //#[allow(unused)]
-    //let module = MModule::new(name, wit_section_bytes).unwrap();
     let mut map = HashMap::new();
     map.insert(name.to_string(), Vec::<u8>::from(wit_section_bytes));
     let faas = FluenceFaaS::with_modules(map).unwrap();
@@ -146,129 +101,8 @@ pub fn register_module(name: &str, wit_section_bytes: &[u8], wasm_instance: JsVa
     MODULES.with(|modules| modules.replace(Some(faas)));
 
     INSTANCE.with(|instance| instance.replace(Some(wasm_instance)));
-    /*
-    INSTANCE.with(|instance| {
-        instance.borrow().as_ref().unwrap().to_string();
-    });*/
 }
 
-/*
-#[wasm_bindgen]
-pub fn call(module_name: &str, function_name: &str, args: &str) -> String {
-    MODULES.with(|modules| -> String {
-        let mut modules = modules.borrow_mut();
-        let module = match modules.get_mut(module_name) {
-            Some(module) => module,
-            None => {
-                js_log(&format!(r#"No "{}" module in registered"#, module_name));
-                unreachable!();
-            }
-        };
-
-        let args = serde_json::de::from_str::<Vec<IValue>>(args).unwrap();
-
-        let output = match module.call(
-            module_name,
-            function_name,
-            &args,
-        ) {
-            Ok(output) => output,
-            Err(e) => {
-                crate::js_log(&format!(r#"{}.{} call error: {}"#, module_name, function_name, e));
-                unreachable!();
-            }
-        };
-
-        serde_json::ser::to_string(&output).unwrap()
-            /*
-        for out in output {
-            js_log(&format!("got output: {}", ival_to_string(&out)));
-        }*/
-    })
-}
-*/
-/*
-#[wasm_bindgen]
-pub fn test_call_avm() {
-    MODULES.with(|modules| {
-        let mut modules = modules.borrow_mut();
-        let module = match modules.get_mut("avm") {
-            Some(module) => module,
-            None => {
-                js_log("No AVM module in registered");
-                unreachable!();
-            }
-        };
-
-        let vm_peer_id = "some_vm_peer_id";
-
-        let script = format!(
-            r#"
-        (seq
-            (par
-                (call "{0}" ("local_service_id" "local_fn_name") [] result_1)
-                (call "remote_peer_id" ("service_id" "fn_name") [] g)
-            )
-            (call "{0}" ("local_service_id" "local_fn_name") [] result_2)
-        )"#,
-            vm_peer_id
-        );
-        let air = IValue::String(script);
-        let prev_data = IValue::Array(vec![]);
-        let data = IValue::Array(vec![]);
-        let run_parameters = IValue::Record(ne_vec![
-            IValue::String("some_peer_id".to_string()),
-            IValue::String("some_current_peer_id".to_string())
-        ]);
-        let call_results = IValue::ByteArray(Vec::from("{}".as_bytes()));
-
-        let output = match module.call(
-            "avm",
-            "invoke",
-            &[air, prev_data, data, run_parameters, call_results],
-        ) {
-            Ok(output) => output,
-            Err(e) => {
-                crate::js_log(&format!("invoke call error: {}", e));
-                unreachable!();
-            }
-        };
-
-        for out in output {
-            js_log(&format!("got output: {}", ival_to_string(&out)));
-        }
-    })
-}
-
-#[wasm_bindgen]
-pub fn test_call_greeting_array() {
-    MODULES.with(|modules| {
-        let mut modules = modules.borrow_mut();
-        let module = match modules.get_mut("greeting") {
-            Some(module) => module,
-            None => {
-                js_log("No AVM module in registered");
-                unreachable!();
-            }
-        };
-
-        //let data = IValue::Array(vec![IValue::U8(48), IValue::U8(49), IValue::U8(50), IValue::U8(51)]);
-        let data = IValue::ByteArray(vec![48, 49, 50, 51]);
-
-        let output = match module.call("greeting", "greeting_array", &[data]) {
-            Ok(output) => output,
-            Err(e) => {
-                crate::js_log(&format!("invoke call error: {}", e));
-                unreachable!();
-            }
-        };
-
-        for out in output {
-            js_log(&format!("got output: {}", ival_to_string(&out)));
-        }
-    })
-}
-*/
 #[wasm_bindgen]
 pub fn call_module(module_name: &str, function_name: &str, args: &str) -> String {
     js_log("ar123123");
@@ -300,20 +134,3 @@ pub fn call_module(module_name: &str, function_name: &str, args: &str) -> String
         }
     })
 }
-/*
-pub(crate) fn extract_it_from_bytes(wit_section_bytes: &[u8]) -> Result<Interfaces<'_>, MyError> {
-    match wasmer_it::decoders::binary::parse::<(&[u8], nom::error::ErrorKind)>(wit_section_bytes) {
-        Ok((remainder, it)) if remainder.is_empty() => Ok(it),
-        Ok(_) => Err(MyError::ITRemainderNotEmpty),
-        Err(e) => Err(MyError::CorruptedITSection(e.to_string())),
-    }
-}
-
-#[derive(Debug, ThisError)]
-enum MyError {
-    #[error("ITRemainderNotEmpty")]
-    ITRemainderNotEmpty,
-    #[error("CorruptedITSection {0}")]
-    CorruptedITSection(String),
-}
-*/
