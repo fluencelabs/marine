e08d0f26d1752ffb9bbcf1e232100ad591cd041a
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-01-23 23:15:44 +0300

    more JS package stuff

--- web-runtime/js-package/src/__test__/test.spec.ts
+++ web-runtime/js-package/src/__test__/test.spec.ts
@@ -1,5 +1,89 @@
-describe("Tests", () => {
-  it("should work", async () => {
-    expect(1 + 2).toBe(3);
-  });
+import init from '../marine_web_runtime';
+import fs from 'fs';
+import path from 'path';
+import { WASI } from '@wasmer/wasi';
+import { WasmFs } from '@wasmer/wasmfs';
+import nodeBindings from '@wasmer/wasi/lib/bindings/node';
+
+const createModule = async (path: string) => {
+    const file = fs.readFileSync(path);
+    return await WebAssembly.compile(file);
+};
+
+const invokeJson = (air: any, prevData: any, data: any, paramsToPass: any, callResultsToPass: any) => {
+    return JSON.stringify([
+        air,
+        Array.from(prevData),
+        Array.from(data),
+        paramsToPass,
+        Array.from(Buffer.from(JSON.stringify(callResultsToPass))),
+    ]);
+};
+
+const b = (s: string) => {
+    return Buffer.from(s);
+};
+
+const defaultAvmFileName = 'avm.wasm';
+const avmPackageName = '@fluencelabs/avm';
+
+const vmPeerId = '12D3KooWNzutuy8WHXDKFqFsATvCR6j9cj2FijYbnd47geRKaQZS';
+
+const _wasmFs = new WasmFs();
+
+const _wasi = new WASI({
+    // Arguments passed to the Wasm Module
+    // The first argument is usually the filepath to the executable WASI module
+    // we want to run.
+    args: [],
+
+    // Environment variables that are accesible to the WASI module
+    env: {},
+
+    // Bindings that are used by the WASI Instance (fs, path, etc...)
+    bindings: {
+        ...nodeBindings,
+        fs: _wasmFs.fs,
+    },
+});
+
+describe('Tests', () => {
+    it('should work', async () => {
+        const fluencePath = eval('require').resolve(avmPackageName);
+        // const avmPath = path.join(path.dirname(fluencePath), defaultAvmFileName);
+        const avmPath = path.join(__dirname, '../', defaultAvmFileName);
+        const avmModule = await createModule(avmPath);
+
+        const controlModule = await createModule(path.join(__dirname, '../marine-js.wasm'));
+        const control = await init(controlModule);
+
+        const avmInstance = await WebAssembly.instantiate(avmModule, {
+            ..._wasi.getImports(avmModule),
+            host: {
+                log_utf8_string: (level: any, target: any, offset: any, size: any) => {
+                    console.log('logging, logging, logging');
+                },
+            },
+        });
+        const customSections = WebAssembly.Module.customSections(avmModule, 'interface-types');
+        const itcustomSections = new Uint8Array(customSections[0]);
+        control.register_module('avm', itcustomSections, avmInstance);
+
+        const s = `(seq
+            (par 
+                (call "${vmPeerId}" ("local_service_id" "local_fn_name") [] result_1)
+                (call "remote_peer_id" ("service_id" "fn_name") [] g)
+            )
+            (call "${vmPeerId}" ("local_service_id" "local_fn_name") [] result_2)
+        )`;
+
+        const params = { initPeerId: vmPeerId, currentPeerId: vmPeerId };
+        let res: any = control.call_module('avm', 'invoke', invokeJson(s, b(''), b(''), params, []));
+        res = res.result;
+
+        expect(res).toMatchObject({
+            retCode: 0,
+            errorMessage: '',
+        });
+    });
 });
--- web-runtime/js-package/src/marine_web_runtime.js
+++ web-runtime/js-package/src/marine_web_runtime.js
@@ -5,6 +5,119 @@ import {
     get_memory_size,
 } from './snippets/marine-web-runtime-6faa67b8af9cc173/marine-js.js';
 
+/**
+ */
+export class CallModuleResult {
+    static __wrap(wasm, ptr) {
+        const obj = Object.create(CallModuleResult.prototype);
+        obj.ptr = ptr;
+        obj._wasm = wasm;
+
+        return obj;
+    }
+
+    __destroy_into_raw() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        this._wasm.__wbg_callmoduleresult_free(ptr);
+    }
+    /**
+     */
+    get result() {
+        try {
+            const retptr = this._wasm.__wbindgen_add_to_stack_pointer(-16);
+            this._wasm.__wbg_get_callmoduleresult_result(retptr, this.ptr);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            return getStringFromWasm0(r0, r1);
+        } finally {
+            this._wasm.__wbindgen_add_to_stack_pointer(16);
+            this._wasm.__wbindgen_free(r0, r1);
+        }
+    }
+    /**
+     * @param {string} arg0
+     */
+    set result(arg0) {
+        var ptr0 = passStringToWasm0(arg0, this._wasm.__wbindgen_malloc, this._wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        this._wasm.__wbg_set_callmoduleresult_result(this.ptr, ptr0, len0);
+    }
+    /**
+     */
+    get error() {
+        try {
+            const retptr = this._wasm.__wbindgen_add_to_stack_pointer(-16);
+            this._wasm.__wbg_get_callmoduleresult_error(retptr, this.ptr);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            return getStringFromWasm0(r0, r1);
+        } finally {
+            this._wasm.__wbindgen_add_to_stack_pointer(16);
+            this._wasm.__wbindgen_free(r0, r1);
+        }
+    }
+    /**
+     * @param {string} arg0
+     */
+    set error(arg0) {
+        var ptr0 = passStringToWasm0(arg0, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        _this.wasm.__wbg_set_callmoduleresult_error(this.ptr, ptr0, len0);
+    }
+}
+/**
+ */
+export class RegisterModuleResult {
+    static __wrap(wasm, ptr) {
+        const obj = Object.create(RegisterModuleResult.prototype);
+        obj.ptr = ptr;
+        obj._wasm = wasm;
+
+        return obj;
+    }
+
+    __destroy_into_raw() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        this._wasm.__wbg_registermoduleresult_free(ptr);
+    }
+    /**
+     */
+    get error() {
+        try {
+            const retptr = this._wasm.__wbindgen_add_to_stack_pointer(-16);
+            this._wasm.__wbg_get_callmoduleresult_result(retptr, this.ptr);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            return getStringFromWasm0(r0, r1);
+        } finally {
+            _this.wasm.__wbindgen_add_to_stack_pointer(16);
+            _this.wasm.__wbindgen_free(r0, r1);
+        }
+    }
+    /**
+     * @param {string} arg0
+     */
+    set error(arg0) {
+        var ptr0 = passStringToWasm0(arg0, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        this._wasm.__wbg_set_callmoduleresult_result(this.ptr, ptr0, len0);
+    }
+}
+
 async function init(module) {
     let wasm;
 
@@ -30,10 +143,7 @@ async function init(module) {
         return ret;
     }
 
-    let cachedTextDecoder = new TextDecoder('utf-8', {
-        ignoreBOM: true,
-        fatal: true,
-    });
+    let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
     cachedTextDecoder.decode();
 
@@ -122,14 +232,6 @@ async function init(module) {
         }
         return cachegetInt32Memory0;
     }
-    /**
-     * @param {string} name
-     */
-    function greet(name) {
-        var ptr0 = passStringToWasm0(name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        var len0 = WASM_VECTOR_LEN;
-        wasm.greet(ptr0, len0);
-    }
 
     function passArray8ToWasm0(arg, malloc) {
         const ptr = malloc(arg.length * 1);
@@ -141,38 +243,32 @@ async function init(module) {
      * @param {string} name
      * @param {Uint8Array} wit_section_bytes
      * @param {any} wasm_instance
+     * @returns {RegisterModuleResult}
      */
     function register_module(name, wit_section_bytes, wasm_instance) {
         var ptr0 = passStringToWasm0(name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         var len0 = WASM_VECTOR_LEN;
         var ptr1 = passArray8ToWasm0(wit_section_bytes, wasm.__wbindgen_malloc);
         var len1 = WASM_VECTOR_LEN;
-        wasm.register_module(ptr0, len0, ptr1, len1, addHeapObject(wasm_instance));
+        var ret = wasm.register_module(ptr0, len0, ptr1, len1, addHeapObject(wasm_instance));
+        return RegisterModuleResult.__wrap(wasm, ret);
     }
 
     /**
      * @param {string} module_name
      * @param {string} function_name
      * @param {string} args
-     * @returns {string}
+     * @returns {CallModuleResult}
      */
     function call_module(module_name, function_name, args) {
-        try {
-            const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-            var ptr0 = passStringToWasm0(module_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-            var len0 = WASM_VECTOR_LEN;
-            var ptr1 = passStringToWasm0(function_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-            var len1 = WASM_VECTOR_LEN;
-            var ptr2 = passStringToWasm0(args, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-            var len2 = WASM_VECTOR_LEN;
-            wasm.call_module(retptr, ptr0, len0, ptr1, len1, ptr2, len2);
-            var r0 = getInt32Memory0()[retptr / 4 + 0];
-            var r1 = getInt32Memory0()[retptr / 4 + 1];
-            return getStringFromWasm0(r0, r1);
-        } finally {
-            wasm.__wbindgen_add_to_stack_pointer(16);
-            wasm.__wbindgen_free(r0, r1);
-        }
+        var ptr0 = passStringToWasm0(module_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        var ptr1 = passStringToWasm0(function_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len1 = WASM_VECTOR_LEN;
+        var ptr2 = passStringToWasm0(args, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len2 = WASM_VECTOR_LEN;
+        var ret = wasm.call_module(ptr0, len0, ptr1, len1, ptr2, len2);
+        return CallModuleResult.__wrap(wasm, ret);
     }
 
     async function load(module, imports) {
@@ -208,6 +304,17 @@ async function init(module) {
     async function init(wasmModule) {
         const imports = {};
         imports.wbg = {};
+        imports.wbg.__wbg_writebyte_81064940ca9059c1 = function (arg0, arg1, arg2) {
+            write_byte(getObject(arg0), arg1 >>> 0, arg2);
+        };
+        imports.wbg.__wbg_readbyte_63aea980ce35d833 = function (arg0, arg1) {
+            var ret = read_byte(getObject(arg0), arg1 >>> 0);
+            return ret;
+        };
+        imports.wbg.__wbg_getmemorysize_385fa0bd4e2d9ff6 = function (arg0) {
+            var ret = get_memory_size(getObject(arg0));
+            return ret;
+        };
         imports.wbg.__wbg_new_693216e109162396 = function () {
             var ret = new Error();
             return addHeapObject(ret);
@@ -229,17 +336,6 @@ async function init(module) {
         imports.wbg.__wbindgen_object_drop_ref = function (arg0) {
             takeObject(arg0);
         };
-        imports.wbg.__wbg_writebyte_81064940ca9059c1 = function (arg0, arg1, arg2) {
-            write_byte(getObject(arg0), arg1 >>> 0, arg2);
-        };
-        imports.wbg.__wbg_readbyte_63aea980ce35d833 = function (arg0, arg1) {
-            var ret = read_byte(getObject(arg0), arg1 >>> 0);
-            return ret;
-        };
-        imports.wbg.__wbg_getmemorysize_385fa0bd4e2d9ff6 = function (arg0) {
-            var ret = get_memory_size(getObject(arg0));
-            return ret;
-        };
         imports.wbg.__wbg_callexport_cb1a6ee1197892bd = function (arg0, arg1, arg2, arg3, arg4, arg5) {
             var ret = call_export(getObject(arg1), getStringFromWasm0(arg2, arg3), getStringFromWasm0(arg4, arg5));
             var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
@@ -247,8 +343,8 @@ async function init(module) {
             getInt32Memory0()[arg0 / 4 + 1] = len0;
             getInt32Memory0()[arg0 / 4 + 0] = ptr0;
         };
-        imports.wbg.__wbg_alert_607ccf701013ae06 = function (arg0, arg1) {
-            console.log(getStringFromWasm0(arg0, arg1));
+        imports.wbg.__wbindgen_throw = function (arg0, arg1) {
+            throw new Error(getStringFromWasm0(arg0, arg1));
         };
 
         const instance = await WebAssembly.instantiate(wasmModule, imports);
@@ -263,9 +359,9 @@ async function init(module) {
     await init(module);
 
     return {
+        wasm: wasm,
         register_module,
         call_module,
-        greet,
     };
 }
 
