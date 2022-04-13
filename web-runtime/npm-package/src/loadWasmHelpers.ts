98a8225897d871dbf848d24300cd41c236e9d11b
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-04-13 14:52:22 +0300

    Add docs

--- web-runtime/npm-package/src/loadWasmHelpers.ts
+++ web-runtime/npm-package/src/loadWasmHelpers.ts
@@ -1,36 +1,49 @@
 import { isBrowser, isNode } from 'browser-or-node';
 import { defaultNames } from '.';
 
-const bufferToSharedArrayBuffer = (buffer: Buffer): SharedArrayBuffer | Buffer => {
-    // only convert to shared buffers if necessary CORS headers have been set:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements
-    if (isBrowser && eval('crossOriginIsolated')) {
-        const sab = new SharedArrayBuffer(buffer.length);
-        const tmp = new Uint8Array(sab);
-        tmp.set(buffer, 0);
-        return sab;
-    } else {
-        return buffer;
-    }
+const bufferToSharedArrayBuffer = (buffer: Buffer): SharedArrayBuffer => {
+    const sab = new SharedArrayBuffer(buffer.length);
+    const tmp = new Uint8Array(sab);
+    tmp.set(buffer, 0);
+    return sab;
 };
 
-export const loadWasmFromServer = async (fileName: string): Promise<SharedArrayBuffer | Buffer> => {
+/**
+ * Load wasm file from the server. Only works in browsers.
+ * The function will try load file into SharedArrayBuffer if the site is cross-origin isolated.
+ * Otherwise the return value fallbacks to Buffer which is less performant but is still compatible with FluenceAppService methods.
+ * We strongly recommend to set-up cross-origin headers. For more details see: See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements
+ * Filename is relative to current origin.
+ * @param filePath - path to the wasm file relative to current origin
+ * @returns Either SharedArrayBuffer or Buffer with the wasm file
+ */
+export const loadWasmFromServer = async (filePath: string): Promise<SharedArrayBuffer | Buffer> => {
     if (!isBrowser) {
         throw new Error('Files can be loaded from url only in browser environment');
     }
 
-    const fullUrl = window.location.origin + '/' + fileName;
+    const fullUrl = window.location.origin + '/' + filePath;
     const res = await fetch(fullUrl);
     const ab = await res.arrayBuffer();
     new Uint8Array(ab);
     const buffer = Buffer.from(ab);
-    return bufferToSharedArrayBuffer(buffer);
+
+    // only convert to shared buffers if necessary CORS headers have been set:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements
+    if (eval('crossOriginIsolated')) {
+        return bufferToSharedArrayBuffer(buffer);
+    }
+
+    return buffer;
 };
 
-export const loadWasmFromNpmPackage = async (source: {
-    package: string;
-    file: string;
-}): Promise<SharedArrayBuffer | Buffer> => {
+/**
+ * Load wasm file from npm package. Only works in nodejs environment.
+ * The function returns SharedArrayBuffer compatible with FluenceAppService methods.
+ * @param source - object specifying the source of the file. Consist two fields: package name and file path.
+ * @returns SharedArrayBuffer with the wasm file
+ */
+export const loadWasmFromNpmPackage = async (source: { package: string; file: string }): Promise<SharedArrayBuffer> => {
     if (!isNode) {
         throw new Error('Files can be loaded from npm packages only in nodejs environment');
     }
@@ -46,7 +59,13 @@ export const loadWasmFromNpmPackage = async (source: {
     return bufferToSharedArrayBuffer(buffer);
 };
 
-export const loadWasmFromFileSystem = async (filePath: string): Promise<SharedArrayBuffer | Buffer> => {
+/**
+ * Load wasm file from the file system. Only works in nodejs environment.
+ * The functions returns SharedArrayBuffer compatible with FluenceAppService methods.
+ * @param filePath - path to the wasm file
+ * @returns SharedArrayBuffer with the wasm file
+ */
+export const loadWasmFromFileSystem = async (filePath: string): Promise<SharedArrayBuffer> => {
     if (!isNode) {
         throw new Error('Files can be loaded from file system only in nodejs environment');
     }
@@ -59,6 +78,13 @@ export const loadWasmFromFileSystem = async (filePath: string): Promise<SharedAr
     return bufferToSharedArrayBuffer(buffer);
 };
 
+/**
+ * Load wasm marine control module and avm module required for marine-js to work from the default source.
+ * Both modules are loaded in the format compatible with FluenceAppService methods.
+ * If called from the nodejs environment files are loaded from corresponding npm packages.
+ * If called inside browser files are loaded from the server.
+ * @returns Object with two fields: "marine" and "avm" corresponding to control module and avm module
+ */
 export const loadDefaults = async (): Promise<{
     marine: SharedArrayBuffer | Buffer;
     avm: SharedArrayBuffer | Buffer;
