bacc51af8cd048a0c9c43a8f6d188ba725f7d49d
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-04-20 01:49:51 +0300

    Sqlite and redis tests (WIP), not working

--- web-runtime/npm-package/src/__test__/test.spec.ts
+++ web-runtime/npm-package/src/__test__/test.spec.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import tmp from 'tmp';
 import download from 'download';
 import { FaaS } from '../FaaS';
 import { callAvm } from '@fluencelabs/avm';
@@ -20,22 +19,12 @@ const loadWasmModule = async (waspPath: string) => {
     return module;
 };
 
-const redisDownloadUrl = 'https://github.com/fluencelabs/redis/releases/download/v0.14.0_w/redis.wasm';
-const sqliteDownloadUrl = 'https://github.com/fluencelabs/sqlite/releases/download/v0.14.0_w/sqlite3.wasm';
+const redisDownloadUrl = 'https://github.com/fluencelabs/redis/releases/download/v0.10.0_w/redis.wasm';
+const sqliteDownloadUrl = 'https://github.com/fluencelabs/sqlite/releases/download/v0.15.0_w/sqlite3.wasm';
 
 const examplesDir = path.join(__dirname, '../../../../examples');
 
-let tempFile: tmp.FileResult;
-
 describe('Fluence app service tests', () => {
-    beforeEach(() => {
-        tempFile = tmp.fileSync();
-    });
-
-    afterEach(() => {
-        tempFile?.removeCallback();
-    });
-
     it('Testing greeting service', async () => {
         // arrange
         const marine = await loadWasmModule(path.join(__dirname, '../../dist/marine-js.wasm'));
@@ -121,12 +110,59 @@ describe('Fluence app service tests', () => {
         const marine = new FaaS(control, sqlite, 'sqlite');
         await marine.init();
 
-        const result1 = marine.call('sqlite3_open_v2', a(':memory:', 6, ''), undefined);
-        console.log(result1);
+        let result;
+
+        result = doCall(marine, 'sqlite3_open_v2', ':memory:', 6, '');
+        const dbHandle = result.db_handle;
+        result = doCall(marine, 'sqlite3_exec', dbHandle, 'CREATE VIRTUAL TABLE users USING FTS5(body)', 0, 0);
+        result = doCall(
+            marine,
+            'sqlite3_exec',
+            dbHandle,
+            "INSERT INTO users(body) VALUES('AB'), ('BC'), ('CD'), ('DE')",
+            0,
+            0,
+        );
+        result = doCall(marine, 'sqlite3_exec', dbHandle, "SELECT * FROM users WHERE users MATCH 'A* OR B*'", 0, 0);
+
+        console.log(result);
+
+        // TODO:
+        expect(1).toBe(2);
+    });
+
+    it('Testing redis wasm', async () => {
+        const control = await loadWasmModule(path.join(__dirname, '../../dist/marine-js.wasm'));
+        const buf = await download(redisDownloadUrl);
+        const redis = await WebAssembly.compile(buf);
+
+        const marine = new FaaS(control, redis, 'redis');
+        await marine.init();
+
+        const result1 = doCall(marine, 'invoke', 'SET A 10');
+        const result2 = doCall(marine, 'invoke', 'SADD B 20');
+        const result3 = doCall(marine, 'invoke', 'GET A');
+        const result4 = doCall(marine, 'invoke', 'SMEMBERS B');
+        const result5 = doCall(
+            marine,
+            'invoke',
+            "eval \"redis.call('incr', 'A') return redis.call('get', 'A') * 8 + 5\"  0",
+        );
+
+        expect(result1).toBe('+OK\r\n');
+        expect(result2).toBe(':1\r\n');
+        expect(result3).toBe('$2\r\n10\r\n');
+        expect(result4).toBe('*1\r\n$2\r\n20\r\n');
+        expect(result5).toBe(':93\r\n');
     });
 });
 
-const a = (...args: any[]) => {
-    console.log(JSON.stringify(args));
-    return JSON.stringify(args);
+const doCall = (marine: FaaS, fn: string, ...args: any[]) => {
+    const argsStr = JSON.stringify(args);
+    const rawRes = marine.call(fn, argsStr, undefined);
+    const res = JSON.parse(rawRes);
+    if (res.error && res.error.length > 0) {
+        throw new Error(`call failed args: ${argsStr}, res: ${rawRes}`);
+    }
+    return res.result;
 };
