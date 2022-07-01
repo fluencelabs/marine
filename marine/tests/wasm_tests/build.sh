ffa4e5b4c3d9b2cdb7b548bdbae63ab7ee703f5a
Anatoly Laskaris <github_me@nahsi.dev>
2022-07-01 15:50:08 +0300

    Update build script

--- marine/tests/wasm_tests/build.sh
+++ marine/tests/wasm_tests/build.sh
@@ -1,30 +1,13 @@
-#/bin/sh
+#! /usr/bin/env bash
 
-# This script builds all tests
-(
-  cd arguments_passing || exit;
-  cargo update --aggressive;
-  marine build --release;
-  rm artifacts/* || true;
-)
-
-(
-  cd arrays_passing || exit;
-  cargo update --aggressive;
-  marine build --release;
-  rm artifacts/* || true;
-)
-
-(
-  cd records_passing || exit;
-  cargo update --aggressive;
-  marine build --release;
-  rm artifacts/* || true;
-)
-
-cp ../../../target/wasm32-wasi/release/arguments_passing_effector.wasm arguments_passing/artifacts/
-cp ../../../target/wasm32-wasi/release/arguments_passing_pure.wasm arguments_passing/artifacts/
-cp ../../../target/wasm32-wasi/release/arrays_passing_effector.wasm arrays_passing/artifacts/
-cp ../../../target/wasm32-wasi/release/arrays_passing_pure.wasm arrays_passing/artifacts/
-cp ../../../target/wasm32-wasi/release/records_passing_effector.wasm records_passing/artifacts/
-cp ../../../target/wasm32-wasi/release/records_passing_pure.wasm records_passing/artifacts/
+# Build all tests
+for test in *; do
+ if [[ -d $test ]]; then
+   echo "Building $test"
+   cd $test
+   cargo update --aggressive
+   marine build --release
+   mkdir artifacts -p
+   cp ../../../../target/wasm32-wasi/release/$test_*.wasm artifacts/
+   cd -
+ fi
