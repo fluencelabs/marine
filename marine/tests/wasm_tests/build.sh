eaa03dd802b3554ef3bafa7a7e4d486d03c63f7e
Anatoly Laskaris <github_me@nahsi.dev>
2022-07-01 15:59:11 +0300

    Fixes

--- marine/tests/wasm_tests/build.sh
+++ marine/tests/wasm_tests/build.sh
@@ -2,12 +2,13 @@
 
 # Build all tests
 for test in *; do
- if [[ -d $test ]]; then
-   echo "Building $test"
-   cd $test
-   cargo update --aggressive
-   marine build --release
-   mkdir artifacts -p
-   cp ../../../../target/wasm32-wasi/release/$test_*.wasm artifacts/
-   cd -
- fi
+  if [[ -d $test ]]; then
+    echo "Building $test"
+    cd $test
+    cargo update --aggressive
+    marine build --release
+    mkdir artifacts -p
+    cp ../../../../target/wasm32-wasi/release/$test_*.wasm artifacts/
+    cd -
+  fi
+done
