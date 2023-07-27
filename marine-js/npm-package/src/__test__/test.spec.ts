367e7b274964758d53365dcccab47c3f4424f9da
Valery Antopol <valery.antopol@gmail.com>
2023-07-27 17:39:19 +0300

    improve test readability

--- marine-js/npm-package/src/__test__/test.spec.ts
+++ marine-js/npm-package/src/__test__/test.spec.ts
@@ -389,8 +389,13 @@ describe('Fluence app service tests', () => {
             tetraplets: tetraplets,
         };
 
-        const expected_result = "init_peer_id\nservice_id\nservice_creator_peer_id\nhost_id\nparticle_id\n[[SecurityTetraplet { peer_pk: \"peer_pk\", service_id: \"service_id\", function_name: \"some_func_name\", json_path: \"some_json_path\" }]]"
-        ;
+        const expected_result =
+            "init_peer_id\n" +
+            "service_id\n" +
+            "service_creator_peer_id\n" +
+            "host_id\n" +
+            "particle_id\n" +
+            "[[SecurityTetraplet { peer_pk: \"peer_pk\", service_id: \"service_id\", function_name: \"some_func_name\", json_path: \"some_json_path\" }]]";
 
         const result1 = marineService.call("call_parameters", [], call_parameters);
         expect(result1).toStrictEqual(expected_result)
