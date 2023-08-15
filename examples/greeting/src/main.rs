5142dde7044821a3b49d0c63a2a9efd229efacde
Valery Antopol <valery.antopol@gmail.com>
2023-08-15 15:12:51 +0300

    chore: remove marine-rs-sdk-test usages

--- examples/greeting/src/main.rs
+++ examples/greeting/src/main.rs
@@ -25,20 +25,3 @@ pub fn main() {}
 pub fn greeting(name: String) -> String {
     format!("Hi, {}", name)
 }
-
-#[cfg(test)]
-mod tests {
-    use marine_rs_sdk_test::marine_test;
-
-    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
-    fn empty_string(greeting: marine_test_env::greeting::ModuleInterface) {
-        let actual = greeting.greeting(String::new());
-        assert_eq!(actual, "Hi, ");
-    }
-
-    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
-    fn non_empty_string(greeting: marine_test_env::greeting::ModuleInterface) {
-        let actual = greeting.greeting("name".to_string());
-        assert_eq!(actual, "Hi, name");
-    }
-}
