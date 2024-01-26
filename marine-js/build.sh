4a51bd2dd982f23413d2978c5cd95ea857545055
Valery Antopol <valery.antopol@gmail.com>
2024-01-26 19:19:06 +0400

    try locking deps

--- marine-js/build.sh
+++ marine-js/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-wasm-pack build -d marine-js-pkg --target web
+wasm-pack build -d marine-js-pkg --target web --locked
 
 MARINE_JS_JS_DEST=npm-package/src/snippets/marine-js-6faa67b8af9cc173/
 mkdir -p $MARINE_JS_JS_DEST
